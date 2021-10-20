0775a04514098961b007edf57536b3628c930791
Victor M Reyes Espinoza <vmreyes@visiongpu49.csail.mit.edu>
2021-10-19 22:23:46 -0400

    updated video to frames script for remote pc

--- util/subject_videos_to_frames.sh
+++ util/subject_videos_to_frames.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-VIDEOS="/home/vmreyes/keypointgan/datasets/simple_human36m/human_images/S1/Videos/*"
+VIDEOS_PATH="/data/vision/torralba/scratch/vmreyes/keypointgan/datasets/simple_human36m/human_images/S1/Videos/"
+VIDEOS="/data/vision/torralba/scratch/vmreyes/keypointgan/datasets/simple_human36m/human_images/S1/Videos/*"
 detox -r $VIDEOS
-VIDEO_OUT="/home/vmreyes/keypointgan/datasets/simple_human36m/human_images/S1/Videos/"
+rm $VIDEOS_PATH/*ALL*
+VIDEO_OUT="/data/vision/torralba/scratch/vmreyes/keypointgan/datasets/simple_human36m/human_images/S1/Videos/"
 
 for f in $VIDEOS
 do
@@ -10,9 +12,9 @@ do
 	filename_with_path_activity_and_id="${f%.*}"
 	filename_no_path=$(basename -- "$f")
 	filename_no_path_with_id="${filename_no_path%*.*}"
-	echo "$filename_with_path_activity_and_id"
-	echo "$filename_no_path"
-	echo "$filename_no_path_with_id"
+	echo "filename with path activity and id: $filename_with_path_activity_and_id"
+	echo "filename with no path: $filename_no_path"
+	echo "filename with no path with id: $filename_no_path_with_id"
 	mkdir $filename_with_path_activity_and_id
 	ffmpeg -i $f -vf fps=50 $filename_with_path_activity_and_id/frame-%06d.png
 done
